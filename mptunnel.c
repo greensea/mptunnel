2e4ba0bf4caa6678ad9f832cc1424c819535bf42
greensea <gs@bbxy.net>
2014-08-31 15:16:50 +0800

    完成原型

--- mptunnel.c
+++ mptunnel.c
@@ -14,18 +14,14 @@ static int* g_received_id = NULL;
 /**
  * 组装一个数据包，组装出来的包需要调用 packet_free 进行释放
  */
-packet_t* packet_make(enum packet_type type, const char* buf, int buflen) {
-    static int packet_id = 0;
+packet_t* packet_make(enum packet_type type, const char* buf, int buflen, int id) {
     packet_t* p;
-    
-    if (g_packet_id == NULL) {
-        g_packet_id = &packet_id;
-    }
+
     
     p = (packet_t*)malloc(sizeof(*p) + buflen);
     memset(p, 0x00, sizeof(*p));
     p->type = type;
-    p->id = ++packet_id;
+    p->id = id;
     p->buflen = buflen;
     memcpy(p + 1, buf, buflen);
     
@@ -41,10 +37,10 @@ int packet_free(packet_t* p) {
     
     
     
-int packet_send(int fd, char* buf, int buflen) {
+int packet_send(int fd, char* buf, int buflen, int id) {
     int sendb;
     
-    packet_t* p = packet_make(PKT_TYPE_DATA, buf, buflen);
+    packet_t* p = packet_make(PKT_TYPE_DATA, buf, buflen, id);
 
     sendb = send(fd, p, sizeof(*p) + p->buflen, MSG_DONTWAIT);
     if (sendb < 0) {
@@ -54,7 +50,7 @@ int packet_send(int fd, char* buf, int buflen) {
         LOGW("fd=%d 可能断开了连接\n", fd);
     }
     else {
-        LOGD("向发送了 %d 字节消息“%s”\n", sendb, (char*)(p + 1));
+        LOGD("向 %d 发送了 %d 字节消息“%s”\n", fd, sendb, (char*)(p + 1));
     }
     
     packet_free(p);
